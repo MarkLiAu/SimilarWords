a9eaa79a6887ba45973319fb0613ae137a443a35
Mark Li - AzWinTrial <libaojian@outlook.com>
2021-10-08 16:48:32 +1100

    test 2

--- SimilarWordWeb/Startup.cs
+++ SimilarWordWeb/Startup.cs
@@ -22,7 +22,7 @@ namespace SimilarWordWeb
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
