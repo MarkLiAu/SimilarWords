81b31aaf79a1a5e31aa19f950a661a288d229fb4
MarkLi-desktop <bjli508@outlook.com>
2025-01-01 17:25:42 +1100

    word details

--- src/webapp-angular/src/app/views/search/word-details/word-details.component.ts
+++ src/webapp-angular/src/app/views/search/word-details/word-details.component.ts
@@ -2,12 +2,13 @@ import { Component, computed, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
 import { Word } from '../../../domain/model/word';
 
 @Component({
   selector: 'app-word-details',
-  imports: [MatIconModule, RouterLink, MatDividerModule, MatCardModule],
+  imports: [MatIconModule, RouterLink, MatDividerModule, MatCardModule,MatTooltipModule],
   templateUrl: './word-details.component.html',
   styleUrl: './word-details.component.scss'
 })
@@ -15,16 +16,16 @@ export class WordDetailsComponent {
   word = input.required<Word>();
   similarWords = computed(() => this.word().similarWords?.trim().split(" "));
   dictionaryLinks = computed(() => [
-    { name: "Cambridge", url: `https://dictionary.cambridge.org/dictionary/english/${this.word().name}` },
-    { name: "Merriam-Webster", url: `https://www.merriam-webster.com/dictionary/${this.word().name}` },
-    { name: "Oxford", url: `https://www.oxfordlearnersdictionaries.com/definition/english/${this.word().name}` },
     { name: "Collins", url: `https://www.collinsdictionary.com/dictionary/english/${this.word().name}` },
     { name: "Longman", url: `https://www.ldoceonline.com/dictionary/${this.word().name}` },
-    { name: "iCiba", url: `https://www.iciba.com/word?w=${this.word().name}` },
-    { name: "Google", url: `https://www.google.com/search?q=define+${this.word().name}` },
+    { name: "Cambridge", url: `https://dictionary.cambridge.org/dictionary/english/${this.word().name}` },
+    { name: "Oxford", url: `https://www.oxfordlearnersdictionaries.com/definition/english/${this.word().name}` },
+    { name: "Merriam-Webster", url: `https://www.merriam-webster.com/dictionary/${this.word().name}` },
   ]);
-  ngOninit() {
-    console.log("similarWords: ", this.similarWords());  
 
-  }
+  playSound = (url:string|undefined) => {
+    if(!url) return;
+    let mySound = new Audio(url);
+    mySound.play().catch(e=>console.log('failed to play sound'));
+}
 }
--- src/webapp-angular/src/environments/environment.ts
+++ src/webapp-angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: '/api',
-    useTestData: false
+    useTestData: true
 };
